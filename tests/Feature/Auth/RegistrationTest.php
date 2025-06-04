8d5f9201310ab2c028532764b52d8d2285747700
Modestas Vaitkevičius <info@modestasv.lt>
2025-06-04 10:08:34 +0300

    Beginner challenge code

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -3,17 +3,17 @@
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
 
-    $response->assertStatus(200);
+    $response->assertStatus(404);
 });
 
-test('new users can register', function () {
-    $response = $this->post('/register', [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
-    ]);
+// test('new users can register', function () {
+//     $response = $this->post('/register', [
+//         'name' => 'Test User',
+//         'email' => 'test@example.com',
+//         'password' => 'password',
+//         'password_confirmation' => 'password',
+//     ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
-});
+//     $this->assertAuthenticated();
+//     $response->assertRedirect(route('dashboard', absolute: false));
+// });
